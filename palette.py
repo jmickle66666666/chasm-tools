f1e4e9e0f619c9c8faae54bb47725e3af0e977d6
jazz mickle <jaymickle@gmail.com>
2018-03-03 19:50:25 +0100

    Palette file reader

---
+++ palette.py
@@ -0,0 +1,16 @@
+from PIL import Image
+
+
+class Palette:
+    def __init__(self, data):
+        self.colors = []
+        for i in range(256):
+            self.colors.append((data[(i * 3) + 0] * 4,
+                                data[(i * 3) + 1] * 4,
+                                data[(i * 3) + 2] * 4))
+
+    def to_image(self):
+        img = Image.new("RGBA", (16,16), "white")
+        for i in range(256):
+            img.putpixel((i % 16, i // 16), self.colors[i])
+        return img
